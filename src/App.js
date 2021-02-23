aacf742aaad9977eb51c7ffed679c5d73774a8b6
HusamAjour <husamaj278@gmail.com>
2021-02-23 16:53:32 +0200

    add dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-
+import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src='' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Dashboard />
     </div>
   );
 }
