b7186abb2afd8728e26c4410d0183becd07e0478
Abdulrahman Al-Janabi <aaljanabi1992@gmail.com>
2021-02-25 12:14:45 +0200

    Revert "Sign in"

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AuthProvider from './context/SignInContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
     <App />
-    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
